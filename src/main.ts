4891648cadad9d7b549f918d7d7920ab75f0ab90
KwonMinSeok22 <vkffkelstm1020@gmail.com>
2024-12-31 02:39:18 +0900

    Add global prefix for API

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.setGlobalPrefix('api'); // 전역 접두사 설정
 
   // CORS 설정
   app.enableCors({
