8265e454d1c5c23cdf3ee0500a28c6d365812d57
KwonMinSeok22 <vkffkelstm1020@gmail.com>
2024-12-31 02:42:42 +0900

    Refactor global prefix and CORS settings in main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   app.setGlobalPrefix('api'); // 전역 접두사 설정
 
   // CORS 설정
   app.enableCors({
     origin: [
-      "http://localhost:5173",   // 로컬 개발 서버
-      "https://dnn-corp.pages.dev", // 실제 배포 URL
       "*"
     ],
     methods: ["GET", "POST", "OPTIONS", "PUT", "DELETE"],
